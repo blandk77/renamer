6e8780e07869632e212016ade6e8d985cd3246f4
blandk77 <alexags1983@gmail.com>
2025-04-06 14:02:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,32 +10,38 @@
 
 import re, os, time id_pattern = re.compile(r'^.\d+$')
 
-class Config(object): # AniHorizon client config API_ID = os.environ.get("API_ID", "") API_HASH = os.environ.get("API_HASH", "") BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+class Config(object): 
+  
+
+#client config
+API_ID = os.environ.get("API_ID", "27394279") 
+API_HASH = os.environ.get("API_HASH", "90a9aa4c31afa3750da5fd686c410851") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7567477886:AAEMI6V1ImkbEkwUIkMfHNfVrQFFB4GKNtI")
 
 # premium account string session required 😢   
 STRING_SESSION = os.environ.get("STRING_SESSION", "")  
 
 # database config  
 DB_NAME = os.environ.get("DB_NAME","cluster0")       
-DB_URL = os.environ.get("DB_URL","")  
+DB_URL = os.environ.get("DB_URL","mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse")  
 
 # other configs  
 RKN_PIC = os.environ.get("RKN_PIC", "https://files.catbox.moe/6afj23.jpg")  
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6705898491').split()]  
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002283909105"))  
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7465574522').split()]  
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002288135729"))  
 
 # free upload limit   
 FREE_UPLOAD_LIMIT = 6442450944 # 6GB  
 
 # premium mode feature ✅  
-UPLOAD_LIMIT_MODE = True   
-PREMIUM_MODE = True   
+UPLOAD_LIMIT_MODE = True
+PREMIUM_MODE = True
 
 # force subs  
 try:  
     FORCE_SUB = int(os.environ.get("FORCE_SUB", ""))   
 except:  
-    FORCE_SUB = os.environ.get("FORCE_SUB", "Digital_Botz")  
+    FORCE_SUB = os.environ.get("FORCE_SUB", "The_TGguy")  
 
 # web response configuration       
 PORT = int(os.environ.get("PORT", "8080"))  
