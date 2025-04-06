16f8eb7869f01646ec1ce170f7d4b38897575878
MythicMosaic <tharunfitfun8@gmail.com>
2025-04-06 11:15:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-#Apache License 2.0
-#Copyright (c) 2022 @AniHorizon 
-#Telegram Link : https://t.me/AniHorizon 
-#Repo Link : https://github.com/MythicMosaic/4GB-Renamer-bot-With-metadata-
-#License Link : https://github.com/MythicMosaic/4GB-Renamer-bot-With-metadata-/tree/main/LICENSE
-"""
+# Apache License 2.0
+# Copyright (c) 2022 @AniHorizon 
+# Telegram Link : https://t.me/AniHorizon 
+# Repo Link : https://github.com/MythicMosaic/4GB-Renamer-bot-With-metadata-
+# License Link : https://github.com/MythicMosaic/4GB-Renamer-bot-With-metadata-/tree/main/LICENSE
 
-import re, os, time
+import re
+import os
+import time
+
 id_pattern = re.compile(r'^.\d+$') 
 
 class Config(object):
@@ -14,45 +16,45 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-    # premium account string session required 😢 
+    # Premium account string session required
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
-    # database config
-    DB_NAME = os.environ.get("DB_NAME","cluster0")     
-    DB_URL = os.environ.get("DB_URL","")
+    # Database config
+    DB_NAME = os.environ.get("DB_NAME", "cluster0")     
+    DB_URL = os.environ.get("DB_URL", "")
  
-    # other configs
+    # Other configs
     RKN_PIC = os.environ.get("RKN_PIC", "https://files.catbox.moe/6afj23.jpg")
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6705898491').split()]
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002283909105"))
 
-    # free upload limit 
-    FREE_UPLOAD_LIMIT = 6442450944 # calculation 6*1024*1024*1024=results
+    # Free upload limit 
+    FREE_UPLOAD_LIMIT = 6442450944  # 6 * 1024 * 1024 * 1024
 
-    # premium mode feature ✅
+    # Premium mode features
     UPLOAD_LIMIT_MODE = True 
     PREMIUM_MODE = True 
     
-    #force subs
+    # Force subscribe
     try:
-        FORCE_SUB = int(os.environ.get("FORCE_SUB", "")) 
+        FORCE_SUB = int(os.environ.get("FORCE_SUB", ""))
     except:
         FORCE_SUB = os.environ.get("FORCE_SUB", "Digital_Botz")
         
-    # wes response configuration     
+    # Web response configuration     
     PORT = int(os.environ.get("PORT", "8080"))
     BOT_UPTIME = time.time()
 
 class rkn(object):
-    # part of text configuration
-    START_TXT ="""<b><blockquote>ʜᴇʏ !! {mention}
+    # Part of text configuration
+    START_TXT = """<b><blockquote>ʜᴇʏ !! {mention}
 
 ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀғᴜʟ ғɪʟᴇ ʀᴇɴᴀᴍᴇʀ ʙᴏᴛ~  
 ɪ ᴄᴀɴ ʀᴇɴᴀᴍᴇ ᴀɴʏ ᴠɪᴅᴇᴏs, ᴍᴏᴠɪᴇs, ᴀɴɪᴍᴇs, ᴏʀ ᴀɴʏ ғɪʟᴇs ʏᴏᴜ sᴇɴᴅ~
 
 ᴊᴜsᴛ sᴇɴᴅ ᴀɴʏ ғɪʟᴇ ᴀɴᴅ ᴘɪᴄᴋ ᴀ ɴᴇᴡ ɴᴀᴍᴇ ғᴏʀ ɪᴛ!</blockquote></b>"""
-# -*- coding: utf-8 -*
-ABOUT_TXT = """<b><blockquote>
+
+    ABOUT_TXT = """<b><blockquote>
 Owner     : <a href='https://t.me/Momo_Ayase_bot'>This Person</a>
 Library   : <a href='https://github.com/pyrogram'>Pyrogram</a>
 Language  : <a href='https://www.python.org'>Python 3</a>
@@ -62,14 +64,14 @@ Developer : <a href='https://t.me/Momo_Ayase_bot'>STRYKER</a>
 </blockquote></b>"""
 
     HELP_TXT = """
- /start Tʜᴇ Bᴏᴛ.
+/start Tʜᴇ Bᴏᴛ.
 
 <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
- Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nᴀᴍᴇ \nAɴᴅ Sᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [document, video, audio].           
 ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Momo_Ayase_bot>For Support</a>
 """
 
-    UPGRADE_PREMIUM= """
+    UPGRADE_PREMIUM = """
 •⪼ ★𝘗𝘭𝘢𝘯𝘴    -  ⏳𝘋𝘢𝘵𝘦 - 💸𝘗𝘳𝘪𝘤𝘦 
 •⪼ 🥉𝘉𝘳𝘰𝘯𝘻𝘦  -   3𝘥𝘢𝘺𝘴 -   39
 •⪼ 🥈𝘚𝘪𝘭𝘷𝘦𝘳   -   7𝘥𝘢𝘺𝘴 -   59
@@ -79,9 +81,9 @@ Developer : <a href='https://t.me/Momo_Ayase_bot'>STRYKER</a>
 
 - 𝘋𝘢𝘪𝘭𝘺 𝘜𝘱𝘭𝘰𝘢𝘥 𝘓𝘪𝘮𝘪𝘵 𝘜𝘯𝘭𝘪𝘮𝘪𝘵𝘦𝘥
 - 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
-    """
-    
-    UPGRADE_PLAN= """
+"""
+
+    UPGRADE_PLAN = """
 𝘗𝘭𝘢𝘯: 𝘗𝘳𝘰
 𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
 𝘗𝘳𝘪𝘤𝘦: 179
@@ -93,16 +95,17 @@ Developer : <a href='https://t.me/Momo_Ayase_bot'>STRYKER</a>
 𝘓𝘪𝘮𝘪𝘵: 1000 𝘎𝘉
 
 - 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
-    """
-    
+"""
+
     THUMBNAIL = """
 🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
 
 <b>•></b> Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
-<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
 """
-    CAPTION= """
+
+    CAPTION = """
 📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
 
 <b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
@@ -113,6 +116,7 @@ Exᴀᴍᴩʟᴇ:- `/set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
 💾 Sɪᴢᴇ: {filesize}
 ⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`
 """
+
     BOT_STATUS = """
 ⚡️ ʙᴏᴛ sᴛᴀᴛᴜs ⚡️
 
@@ -120,88 +124,4 @@ Exᴀᴍᴩʟᴇ:- `/set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
 👭 ᴛᴏᴛᴀʟ ᴜsᴇʀꜱ: `{}`
 💸 ᴛᴏᴛᴀʟ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs: `{}`
 ֍ ᴜᴘʟᴏᴀᴅ: `{}`
-⊙ ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
 """
-    LIVE_STATUS = """
-⚡ ʟɪᴠᴇ sᴇʀᴠᴇʀ sᴛᴀᴛᴜs ⚡
-
-ᴜᴘᴛɪᴍᴇ: `{}`
-ᴄᴘᴜ: `{}%`
-ʀᴀᴍ: `{}%` 
-ᴛᴏᴛᴀʟ ᴅɪsᴋ: `{}`
-ᴜsᴇᴅ sᴘᴀᴄᴇ: `{} {}%`
-ғʀᴇᴇ sᴘᴀᴄᴇ: `{}`
-ᴜᴘʟᴏᴀᴅ: `{}`
-ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
-V𝟹.𝟶.𝟶 [STABLE]
-"""
-    DIGITAL_METADATA = """
-❪ SET CUSTOM METADATA ❫
-
-- /metadata - Tᴏ Sᴇᴛ & Cʜᴀɴɢᴇ ʏᴏᴜʀ ᴍᴇᴛᴀᴅᴀᴛᴀ ᴄᴏᴅᴇ
-
-☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
-
-`--change-title @AniHorizon 
---change-video-title @AniHorizon 
---change-audio-title @AniHorizon 
---change-subtitle-title @AniHorizon 
---change-author @AniHorizon `
-
-📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Momo_Ayase_bot
-"""
-    
-    CUSTOM_FILE_NAME = """
-<u>🖋️ Custom File Name</u>
-
-you can pre-add a prefix and suffix along with your new filename
-
-➢ /set_prefix - To add a prefix along with your _filename.
-➢ /see_prefix - Tᴏ Sᴇᴇ Yᴏᴜʀ Pʀᴇғɪx !!
-➢ /del_prefix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Pʀᴇғɪx !!
-➢ /set_suffix - To add a suffix along with your filename_.
-➢ /see_suffix - Tᴏ Sᴇᴇ Yᴏᴜʀ Sᴜғғɪx !!
-➢ /del_suffix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Sᴜғғɪx !!
-
-Exᴀᴍᴩʟᴇ:- `/set_suffix @AniHorizon `
-Exᴀᴍᴩʟᴇ:- `/set_prefix @AniHorizon `
-"""
-    
-    #⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-#⚠️ Dᴏɴ'ᴛ Rᴇᴍᴏᴠᴇ Oᴜʀ Cʀᴇᴅɪᴛꜱ @AniHorizon 🙏🥲
-    # ᴡʜᴏᴇᴠᴇʀ ɪs ᴅᴇᴘʟᴏʏɪɴɢ ᴛʜɪs ʀᴇᴘᴏ ɪs ᴡᴀʀɴᴇᴅ ⚠️ ᴅᴏ ɴᴏᴛ ʀᴇᴍᴏᴠᴇ ᴄʀᴇᴅɪᴛs ɢɪᴠᴇɴ ɪɴ ᴛʜɪs ʀᴇᴘᴏ #ғɪʀsᴛ ᴀɴᴅ ʟᴀsᴛ ᴡᴀʀɴɪɴɢ ⚠️
-    DEV_TXT = """<b><u>Sᴩᴇᴄɪᴀʟ Tʜᴀɴᴋꜱ & Dᴇᴠᴇʟᴏᴩᴇʀꜱ</b></u>
-    
-» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/MythicMosaic/4GB-Renamer-bot-With-metadata-</a>
-
-• ❣️ <a href=https://github.com/MythicMosaic>MythicMosaic</a>
-• ❣️ <a href=https://github.com/MythicMosaic>AniHorizon</a>
-• ❣️ <a href=https://github.com/a2-b3c4d>Stryker</a> """
-    # ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-
-    SEND_METADATA = """
-❪ SET CUSTOM METADATA ❫
-
-☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
-
-`--change-title @AniHorizon 
---change-video-title @AniHorizon 
---change-audio-title @AniHorizon 
---change-subtitle-title @AniHorizon 
---change-author @AniHorizon `
-
-📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Momo_Ayase_bot 
-"""
-    
-    Ani_PROGRESS = """<b>\n
-╭━━━━❰Ani PROCESSING...❱━➣
-┣⪼ 🗃️ ꜱɪᴢᴇ: {1} | {2}
-┣⪼ ⏳️ ᴅᴏɴᴇ : {0}%
-┣⪼ 🚀 ꜱᴩᴇᴇᴅ: {3}/s
-┣⪼ ⏰️ ᴇᴛᴀ: {4}
-╰━━━━━━━━━━━━━━━➣ </b>"""
-
-# Define Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @AniHorizon 
-# Developer @Tharun_stryker 
