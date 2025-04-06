a376035d581078f2f1cfd24fcc9715591a99144b
MythicMosaic <tharunfitfun8@gmail.com>
2025-04-06 11:00:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-Apache License 2.0
-Copyright (c) 2022 @AniHorizon 
+#Apache License 2.0
+#Copyright (c) 2022 @AniHorizon 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
